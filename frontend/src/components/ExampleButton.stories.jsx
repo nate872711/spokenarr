24f649cd261fbb8bc85284f8a51614955b418337
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-02 12:56:36 -0600

    Create ExampleButton.stories.jsx

---
+++ frontend/src/components/ExampleButton.stories.jsx
@@ -0,0 +1,15 @@
+import { ExampleButton } from "./ExampleButton";
+
+export default {
+  title: "Example/ExampleButton",
+  component: ExampleButton,
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const Primary = {
+  args: {
+    label: "Click Me",
+  },
+};
