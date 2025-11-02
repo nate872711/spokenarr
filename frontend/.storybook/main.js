be5c6cd5179d4c13d2ceb9d4b564a97c3c958f67
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-02 13:04:36 -0600

    Update main.js

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,17 +1,14 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+/** @type { import('@storybook/react').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
+  framework: "@storybook/react",
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: true
+  }
 };
 export default config;
