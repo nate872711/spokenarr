f07faf589c3615784fa994df9d2455386ae3619d
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-03 16:02:45 -0600

    Create tailwind.config.js

---
+++ frontend/.storybook/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // Required for theme switching
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
