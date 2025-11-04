03ce92cd80b5c50920f1c50235e8c139eb96a322
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-04 13:54:54 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Library from "./pages/Library";
 import Discover from "./pages/Discover";
 import Downloads from "./pages/Downloads";
@@ -8,11 +9,12 @@ import Settings from "./pages/Settings";
 export default function App() {
   return (
     <Router>
-      <div className="app min-h-screen bg-gray-900 text-gray-100">
+      <div className="bg-gray-900 min-h-screen text-white">
         <Navbar />
-        <main className="p-6">
+        <main className="p-8">
           <Routes>
-            <Route path="/" element={<Library />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/library" element={<Library />} />
             <Route path="/discover" element={<Discover />} />
             <Route path="/downloads" element={<Downloads />} />
             <Route path="/settings" element={<Settings />} />
