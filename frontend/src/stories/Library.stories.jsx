df1836c8ee5618e7cde8145d5439c4f87956b0a8
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-04 16:15:20 -0600

    Create Library.stories.jsx

---
+++ frontend/src/stories/Library.stories.jsx
@@ -0,0 +1,18 @@
+import Library from '../pages/Library';
+
+export default {
+  title: 'Pages/Library',
+  component: Library,
+};
+
+export const Default = () => <Library />;
+
+// frontend/src/stories/Downloads.stories.jsx
+import Downloads from '../pages/Downloads';
+
+export default {
+  title: 'Pages/Downloads',
+  component: Downloads,
+};
+
+export const Default = () => <Downloads />;
