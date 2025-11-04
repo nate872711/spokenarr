2965bc2537a6dc230f51c77522a3ef3067b10ce2
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-04 15:45:44 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Home from "./pages/Home";
-import Library from "./pages/Library";
-import Discover from "./pages/Discover";
-import Downloads from "./pages/Downloads";
-import Settings from "./pages/Settings";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Navbar from './components/Navbar';
+import Discover from './pages/Discover';
+import Library from './pages/Library';
+import Downloads from './pages/Downloads';
+import Settings from './pages/Settings';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="library" element={<Library />} />
-          <Route path="discover" element={<Discover />} />
-          <Route path="downloads" element={<Downloads />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/discover" element={<Discover />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/downloads" element={<Downloads />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
+  </Router>
 );
