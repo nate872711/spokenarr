8efc3182990139537065365430d5d8e47380b16c
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-04 16:14:42 -0600

    Create Home.stories.jsx

---
+++ frontend/src/stories/Home.stories.jsx
@@ -0,0 +1,18 @@
+import App from '../App';
+
+export default {
+  title: 'Pages/Home',
+  component: App,
+};
+
+export const Default = () => <App />;
+
+// frontend/src/stories/Discover.stories.jsx
+import Discover from '../pages/Discover';
+
+export default {
+  title: 'Pages/Discover',
+  component: Discover,
+};
+
+export const Default = () => <Discover />;
