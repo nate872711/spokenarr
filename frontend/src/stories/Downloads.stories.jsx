e6d68d55af5e9fd2cc38204ebe547fe49283045e
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-04 16:18:40 -0600

    Update Downloads.stories.jsx

--- frontend/src/stories/Downloads.stories.jsx
+++ frontend/src/stories/Downloads.stories.jsx
@@ -1,8 +1,18 @@
 import Downloads from '../pages/Downloads';
 
+
 export default {
-  title: 'Pages/Downloads',
-  component: Downloads,
+title: 'Pages/Downloads',
+component: Downloads,
+parameters: {
+layout: 'fullscreen',
+backgrounds: { default: 'dark' },
+},
+argTypes: {
+activeDownloads: { control: 'number', defaultValue: 2 },
+completedDownloads: { control: 'number', defaultValue: 10 },
+},
 };
 
-export const Default = () => <Downloads />;
+
+export const Default = (args) => <Downloads {...args} />;
