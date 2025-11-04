ebef9f639fb99a4995ed8f83ac63fae006fef16b
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-04 14:22:04 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Library from "./pages/Library";
-import Discover from "./pages/Discover";
-import Downloads from "./pages/Downloads";
-import Settings from "./pages/Settings";
 
 export default function App() {
   return (
-    <Router>
-      <div className="bg-gray-900 min-h-screen text-white">
-        <Navbar />
-        <main className="p-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/discover" element={<Discover />} />
-            <Route path="/downloads" element={<Downloads />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1 p-6">
+        <Outlet />
+      </main>
+    </div>
   );
 }
