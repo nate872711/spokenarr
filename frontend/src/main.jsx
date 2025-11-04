048ca9e694118d5e31322f2f3444d60fe53e7a39
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-03 23:47:40 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StoryPage from "./pages/StoryPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
+
+import Navbar from "./Navbar";
+import StoryPage from "./pages/StoryPage";
+import About from "./pages/About";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StoryPage />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<StoryPage />} />
+        <Route path="/stories" element={<StoryPage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
