6ceb826fc644c32de45a204ca21c9d72760be2ed
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-05 09:36:34 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Discover from "./pages/Discover";
 import Library from "./pages/Library";
@@ -10,24 +8,13 @@ import Settings from "./pages/Settings";
 export default function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-background-dark text-gray-100">
-        {/* Navbar */}
-        <Navbar />
-
-        {/* Page Content */}
-        <main className="flex-grow fade-in page-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/discover" element={<Discover />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/downloads" element={<Downloads />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </main>
-
-        {/* Footer */}
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/downloads" element={<Downloads />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
     </Router>
   );
 }
