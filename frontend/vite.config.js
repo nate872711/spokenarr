8cc79ac3d28faffd56548f221bdf9b235282f9c8
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-04 22:32:52 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+  },
   build: {
     outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    proxy: {
-    "/api": {
-      target: "http://api:80",
-      changeOrigin: true,
   },
 });
