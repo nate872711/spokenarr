858eb321d233103e5c9ef0bcf3d95cbdb8b6e9d9
nate872711 <33221612+nate872711@users.noreply.github.com>
2025-11-10 16:48:50 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-import AppLayout from "./components/AppLayout";
-import Home from "./pages/Home";
-import Discover from "./pages/Discover";
-import Library from "./pages/Library";
-import Downloads from "./pages/Downloads";
-import Settings from "./pages/Settings";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "discover", element: <Discover /> },
-      { path: "library", element: <Library /> },
-      { path: "downloads", element: <Downloads /> },
-      { path: "settings", element: <Settings /> },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(<App />)
